ce331fa770c155c1ca99ce4d871d6240475edb56
Cinthia Juarez <gj60818@gmail.com>
2023-11-03 05:37:10 -0300

    Terminado el segundo ejercicio

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,14 +1,33 @@
 const nota = parseInt(prompt("Ingrese un número del 0 al 10"));
 switch (nota) {
   case 0:
-    case 1:
-        case 2:
-
+  case 1:
+  case 2:
     alert("Muy deficiente");
     break;
-    case 3:
-        case 4:
-            alert ("Insuficiente")
-            break;
-            
+  case 3:
+  case 4:
+    alert("Insuficiente");
+    break;
+  case 5:
+  case 6:
+    alert("Suficiente");
+    break;
+  case 7:
+    alert("Bien");
+    break;
+  case 8:
+  case 9:
+    alert("Notable");
+    break;
+  case 10:
+    alert("Sobresaliente");
+    break;
+}
+if (nota > -0 || nota < 10 )
+{
+alert ("Número erroneo")
+} 
+else{
+    alert("Ingrese un número válido")
 }
