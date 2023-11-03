49bdab7325c73dfa7409dfa2c186ded49bbc1144
Cinthia Juarez <gj60818@gmail.com>
2023-11-03 05:01:52 -0300

    termine el primer ejercicio

---
+++ js/ejercicio1.js
@@ -0,0 +1,9 @@
+const edad = parseInt(prompt("Ingrese su edad"));
+if (edad >= 18){
+    alert(
+        "Usted puede manejar"
+    )}else if(edad < 18){
+        alert("Usted no puede manejar")
+    }else{
+        alert ("Vuelva aa ingresar un número válido")
+    }
