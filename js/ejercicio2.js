e9844a2f4794dbc2f495cd43f6414f41cb7893bc
Cinthia Juarez <gj60818@gmail.com>
2023-11-03 05:27:09 -0300

    empece a hacer el switch del ejercicio 2 y agregue las opc de 0-4

---
+++ js/ejercicio2.js
@@ -0,0 +1,14 @@
+const nota = parseInt(prompt("Ingrese un número del 0 al 10"));
+switch (nota) {
+  case 0:
+    case 1:
+        case 2:
+
+    alert("Muy deficiente");
+    break;
+    case 3:
+        case 4:
+            alert ("Insuficiente")
+            break;
+            
+}
