f04c64228835852b1b2c790c90f9efb18b76682b
Cinthia Juarez <gj60818@gmail.com>
2023-11-04 05:17:15 -0300

    terminado el ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,14 @@
+ 
+
+let filas="30"
+let columnas= "30"
+
+for (filas = 1; filas <= 30; filas++) {
+    
+    for (columnas = 0; columnas < filas; columnas++) {
+       
+        document.write(filas);
+    }
+
+    document.write("<br>");
+}
