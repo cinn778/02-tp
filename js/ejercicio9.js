7ab2eeb0ed1cf43250f28f79e18fab63caf693fc
Cinthia Juarez <gj60818@gmail.com>
2023-11-04 07:04:09 -0300

    terminado el ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,17 @@
+
+for (let numero = 1; numero <= 500; numero++){
+    document.write (numero)
+    if (numero % 4 === 0){
+        document.write(" Es multiplo de 4")
+    }
+    if ( numero % 9 === 0){
+        document.write(" Es multiplo de 9")
+    }
+      document.write("<br>")
+ if(
+     numero % 5 ===0
+     ){
+         document.write("<hr>")
+    }
+
+}
